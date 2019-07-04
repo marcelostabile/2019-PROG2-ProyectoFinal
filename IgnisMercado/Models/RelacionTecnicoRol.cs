6b23fc547ffa451e9e455d741b3f6723e2cc7d9d
marcelostabile <marcelo.stabile@correo.ucu.edu.uy>
2019-07-03 21:59:36 -0300

    Versión Completa. Problemas solucionados.

---
+++ IgnisMercado/Models/RelacionTecnicoRol.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using IgnisMercado.Areas.Identity.Data;
+
+namespace IgnisMercado.Models
+{
+    /// <summary>
+    /// Relación Tecnico:Rol.
+    /// </summary>
+    public class RelacionTecnicoRol
+    { 
+        [Key]
+        public string TecnicoId { get; set; }
+
+        [Required]
+        public ApplicationUser Tecnico { get; set; }
+
+        [Key]
+        public int RolId { get; set; }
+        
+        [Required]
+        public Rol Rol { get; set; }
+
+    }     
+}
